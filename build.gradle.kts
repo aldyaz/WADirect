3260ad4805c0c12a6cd481dbc3e6fb3a4d6b6ca6
Aldyaz Nugroho <aldyaz14@gmail.com>
2024-08-04 15:41:33 +0700

    initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.composeCompiler) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(libs.hilt.android.gradle.plugin)
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
+
+tasks.withType(JavaCompile::class.java) {
+    options.compilerArgs.add("-Xlint:deprecation")
+}
